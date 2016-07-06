93e6244499ab22a175177df4b7feff23270b45e6
IwaIwapng <takuma_8.3.6@ezweb.ne.jp>
2016-07-06 09:52:54 +0900

    Playerの更新。移動と二段ジャンプ可能状態でっせ

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,63 @@ public class Player : MonoBehaviour {
 	private Rigidbody2D m_rigidbody;
 	public GameObject m_mainCamera;
 
+	private int m_jump = 2;        //ジャンプの回数
+
 	// Use this for initialization
 	void Start () {
 
 		m_rigidbody = GetComponent<Rigidbody2D> ();
+		m_mainCamera = GameObject.Find ("Main Camera");
+
+		//カメラの位置取得
+		Vector3 cameraPos = m_mainCamera.transform.position;
+		//Playerの位置から右に4つ移動した位置を画面中央に
+		cameraPos.x = transform.position.x + 1;
+		cameraPos.y = transform.position.y + 1;
+		m_mainCamera.transform.position = cameraPos;
 	
+	}
+		
+
+	void Update(){
+
+		//spaceキーを押したら
+		if (Input.GetKeyDown ("space")) {
+			if (m_jump > 0) {
+				this.m_rigidbody.velocity = Vector3.up * 8.0f;
+				m_jump--;
+			}
+		}
+	}
+
+	//地面との当たり判定処理
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Ground") {
+			//二回飛べる制限がリセット
+			m_jump = 2;
+		}
 	}
 
 	void FixedUpdate(){
 
 		float x = Input.GetAxisRaw ("Horizontal");
+
+
+		//画面中央からPlayerが4方向に移動したら
+		if (transform.position.x > m_mainCamera.transform.position.x - 4) {
+			//カメラ位置を取得
+			Vector3 cameraPos = m_mainCamera.transform.position;
+			//Playerの位置から右に4つ移動した位置を画面中央に
+			cameraPos.x = transform.position.x + 1;
+			m_mainCamera.transform.position = cameraPos;
+		}
+
+		if (transform.position.y > m_mainCamera.transform.position.y - 4) {
+			Vector3 cameraPos = m_mainCamera.transform.position;
+			cameraPos.y = transform.position.y + 1;
+			m_mainCamera.transform.position = cameraPos;
+		}
 
 		//入力方向へ移動
 		if (x != 0) {
@@ -27,14 +74,6 @@ public class Player : MonoBehaviour {
 			temp.x = x;
 			transform.localScale = temp;
 
-			//画面中央からPlayerが4方向に移動したら
-			if (transform.position.x > m_mainCamera.transform.position.x - 4) {
-				//カメラ位置を取得
-				Vector3 cameraPos = m_mainCamera.transform.position;
-				//Playerの位置から右に4つ移動した位置を画面中央に
-				cameraPos.x = transform.position.x + 4;
-				m_mainCamera.transform.position = cameraPos;
-			}
 
 			//カメラ表示領域の左下をワールド座標に変換
 			Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
@@ -45,11 +84,13 @@ public class Player : MonoBehaviour {
 			pos.x = Mathf.Clamp (pos.x, min.x + 0.5f, max.x);
 			transform.position = pos;
 
+
 			//キー入力がなければ止まる
 		} else {
 			m_rigidbody.velocity = new Vector2 (0, m_rigidbody.velocity.y);
 
 		}
+
 	}
 	
 }
