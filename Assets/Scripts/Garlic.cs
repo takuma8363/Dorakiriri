31640213f8b12f476fffba5fe05375ef37ccd336
yoshizumi <iwgm14kyoshizumi@gmail.com>
2016-07-11 01:53:15 +0900

    障害物のにんにく。 プレイヤーに当たると、にんにくも消えるように。 ランダムで振ってくる処理は未完成。

---
+++ Assets/Scripts/Garlic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Garlic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //プレイヤーに当たるとにんにくも消えるように
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
