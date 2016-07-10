a3d4b04c6ac0dc24aa3e65ec861155a4a4af8f27
yoshizumi <iwgm14kyoshizumi@gmail.com>
2016-07-11 00:04:09 +0900

    アイテムが消える。 プレイヤーが女性に当たったら女性が消えるけど改良が必要なので作り直します！

---
+++ Assets/Scripts/Female.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Female : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    /*void Update () {
+	
+	}*/
+
+    //プレイヤーに当たったら消える
+    //Garlicのほうと同じで、これだとどのオブジェクトに当たっても消えてしまうので作り直します
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
